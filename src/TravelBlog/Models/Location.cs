1ff6fcc3de8edde010877cb33a3e04b5ab549ca1
brandon cooper and sandro mijatovic <brandon+sandro@>
2017-04-19 16:34:41 -0700

    add project with database

---
+++ src/TravelBlog/Models/Location.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TravelBlog.Models
+{
+    [Table("Locations")]
+    public class Location
+    {
+        [Key]
+        public intLocationId { get; set; }
+        public string Name { get; set; }
+    }
+}
