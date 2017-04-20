e7031f3cc1c543668a3cb9bb2d418198e7a41dc5
brandon cooper and sandro mijatovic <brandon+sandro@>
2017-04-20 08:50:21 -0700

    add location page and detail links

---
+++ src/TravelBlog/Controllers/LocationsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TravelBlog.Models;
+
+namespace TravelBlog.Controllers
+{
+    public class LocationsController : Controller
+    {
+        private TravelBlogContext db = new TravelBlogContext();
+        public IActionResult Index()
+        {
+            return View(db.Locations.ToList());
+        }
+
+        public IActionResult Details(int id)
+        {
+            var thisLocation = db.Locations.FirstOrDefault(locations => locations.LocationId == id);
+            return View(thisLocation);
+        }
+    }
+}
--- src/TravelBlog/Models/Location.cs
+++ src/TravelBlog/Models/Location.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,7 +11,7 @@ namespace TravelBlog.Models
     public class Location
     {
         [Key]
-        public intLocationId { get; set; }
+        public int LocationId { get; set; }
         public string Name { get; set; }
     }
 }
