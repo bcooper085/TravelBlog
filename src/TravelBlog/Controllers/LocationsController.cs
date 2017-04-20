33def294133f9f4e11a486b7263c2e8027c6db91
brandon cooper and sandro mijatovic <brandon+sandro@>
2017-04-20 10:20:05 -0700

    make crud work

--- src/TravelBlog/Controllers/LocationsController.cs
+++ src/TravelBlog/Controllers/LocationsController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using TravelBlog.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace TravelBlog.Controllers
 {
@@ -19,6 +20,48 @@ namespace TravelBlog.Controllers
         {
             var thisLocation = db.Locations.FirstOrDefault(locations => locations.LocationId == id);
             return View(thisLocation);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Location location)
+        {
+            db.Locations.Add(location);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var thisLocation = db.Locations.FirstOrDefault(locations => locations.LocationId == id);
+            return View(thisLocation);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Location location)
+        {
+            db.Entry(location).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var thisLocation = db.Locations.FirstOrDefault(locations => locations.LocationId == id);
+            return View(thisLocation);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var thisLocation = db.Locations.FirstOrDefault(locations => locations.LocationId == id);
+            db.Locations.Remove(thisLocation);
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
